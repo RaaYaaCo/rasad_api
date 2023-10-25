bde3c34ae4cbec7a24414b3b62ecf3f818f977d1
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-10-25 21:34:38 +0330

    Add invoice entry view

--- invoice_entry/serializers.py
+++ invoice_entry/serializers.py
@@ -2,5 +2,27 @@ from rest_framework import serializers
 from . import models
 
 
+class InvoiceEntrySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.InvoiceEntry
+        fields = '__all__'
 
 
+class InvoiceEntryAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.InvoiceEntry
+        fields = '__all__'
+
+
+class InvoiceEntryItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.InvoiceEntryItem
+        fields = '__all__'
+
+
+class InvoiceEntryItemAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.InvoiceEntryItem
+        fields = '__all__'
