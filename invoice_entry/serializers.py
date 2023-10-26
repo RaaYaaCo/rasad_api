6a0f2c46710fe3c074c3e784b1c6fed86d31f5d9
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-10-26 08:46:09 +0330

    Modify invoice entry serializer

--- invoice_entry/serializers.py
+++ invoice_entry/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
+
 from . import models
-
-
-class InvoiceEntrySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.InvoiceEntry
-        fields = '__all__'
+from product.serializers import ProductSerializer
+from user.models import UserGroupOrganization
 
 
 class InvoiceEntryAddSerializer(serializers.ModelSerializer):
@@ -15,14 +11,34 @@ class InvoiceEntryAddSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class InvoiceEntryItemSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.InvoiceEntryItem
-        fields = '__all__'
-
-
 class InvoiceEntryItemAddSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.InvoiceEntryItem
         fields = '__all__'
+
+
+class UserInvoiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserGroupOrganization
+
+
+class InvoiceEntrySerializer(serializers.ModelSerializer):
+    wholesaler_id = UserInvoiceSerializer(read_only=True)
+
+    class Meta:
+        model = models.InvoiceEntry
+        fields = ['id',
+                  'wholesaler_id',
+                  'ie_driver',
+                  'full_weight',
+                  'empty_weight',
+                  'created_at']
+
+
+class InvoiceEntryItemSerializer(serializers.ModelSerializer):
+    p_id = ProductSerializer()
+
+    class Meta:
+        model = models.InvoiceEntryItem
+        fields = ['id', 'invoice_entry_id', 'product_id', 'weight']
