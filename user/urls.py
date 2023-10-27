82f404e13e3e05a881c8902ce280ba5b1d42e7d7
omid <omizare57@gmail.com>
2023-10-27 20:52:58 +0330

    Add user profile

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register-login/', views.RegisterLoginView.as_view(), name='register-login'),
     path('login/password/', views.LoginPasswordView.as_view(), name='login-password'),
     path('login/otp/', views.LoginOtpView.as_view(), name='login-otp'),
+    path('profile/<int:pk>/', views.UserProfileView.as_view(), name='profile')
 ]
